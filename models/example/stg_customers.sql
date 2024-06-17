d3dc9f240f86cd34642d86a204ebe29047778a78
Rajani MOhanty <rajaniblr2022@gmail.com>
2024-06-17 16:08:55 +0000

    Commited to hotfix branch only stg_customers

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,11 @@
+--What is the .sql file name , with same name view or table will be created in db .
+
+With stg_customers AS (
+  SELECT
+      customer_id,
+      CONCAT(CONCAT(first_name,' '),last_name) as customer_name,
+      email as email_address,
+      address as billing_address
+   FROM raw_data.customer
+)
+select * from stg_customers
